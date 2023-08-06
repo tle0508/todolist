a70d95d92b2fc8691a51b89ee291758491b5796f
sirun sununtasin <sirun_s@cmu.ac.th>
2023-08-06 11:28:55 +0700

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo-item';
+import { TodoList } from './todo-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  showComplete = false;
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    //return this.list.items.filter((item) => !item.complete);
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
 }
